8c1cb9b94ccb51c996dbba141d89b97005932cc4
000begana000 <begana.contact@gmail.com>
2025-08-10 14:26:51 +0200

    new project

--- src/components/basics/list/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AppList({ children, className, ...restProps }) {
-    return (
-        <li className={`
-            ${className}
-        `} {...restProps}>{children}</li>
-    )
-}
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as AppButton} from "./basics/button";
-export {default as AppList} from "./basics/list";
-export {default as AppHeader} from "./main/header";
